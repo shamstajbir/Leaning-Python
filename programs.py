ff9f0b99a40c4bd718aec2bfbbbd8f8f3a6c71d8
shamstajbir <25tajbir@gmail.com>
2024-01-31 10:50:58 +0530

    while loop & for loop

---
+++ programs.py
@@ -0,0 +1,12 @@
+
+print("Example of  while loop:")
+count = 0
+while count < 5:
+    print("Count:", count)
+    count += 1
+print("While loop finished.\n")
+
+fruits = ["apple", "banana", "cherry"]
+for fruit in fruits:
+    print(fruit)
+print("For loop finished.")
