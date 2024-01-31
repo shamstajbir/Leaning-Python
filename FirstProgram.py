1aa7fb781a85ca7f5f8ae01a02de22750edfe6dd
shamstajbir <25tajbir@gmail.com>
2024-01-31 10:38:27 +0530

    if else statement

--- FirstProgram.py
+++ FirstProgram.py
@@ -1,6 +1,24 @@
-name="Shams Tajbir "
-age=21
-price=25.99
-print(type(name))
-print(type(age))
-print(type(price))
+
+x = 10
+if x > 5:
+    print("x is greater than 5")
+
+y = 3
+if y % 2 == 0:
+    print("y is even")
+else:
+    print("y is odd")
+
+score = 7
+if score >= 90:
+    grade = 'A'
+elif score >= 80:
+    grade = 'B'
+elif score >= 70:
+    grade = 'C'
+elif score >= 60:
+    grade = 'D'
+else:
+    grade = 'F'
+
+print("Your grade is:", grade)
