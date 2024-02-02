da81a029becf5575b753bcd32aaece213f62e6ed
shamstajbir <25tajbir@gmail.com>
2024-02-03 01:09:25 +0530

    Relational  operators

--- records.py
+++ records.py
@@ -1,23 +1,4 @@
-class Record:
-    def __init__(self, name, age):
-        self.name = name
-        self.age = age
+a=50
+b=20
 
-    def __gt__(self, other):
-        return self.age > other.age
-
-    def get_name(self):
-        return self.name
-
-    def get_age(self):
-        return self.age
-
-
-if __name__ == "__main__":
-    p1 = Record("Shams", 18)
-    p2 = Record("Tajbir", 20)
-
-    if p1 > p2:
-        print(f"{p1.get_name()} is older than {p2.get_name()}")
-    else:
-        print(f"{p2.get_name()} is older than {p1.get_name()}")
+print(a==b)
