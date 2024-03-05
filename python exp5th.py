6943f50828dc814a52f2be41d82b279e983672f0
shamstajbir <25tajbir@gmail.com>
2024-03-05 11:05:39 +0530

    exp5py

---
+++ python exp5th.py
@@ -0,0 +1,21 @@
+# Define the string
+s = "PythonProgramming"
+
+print("Accessing characters by index:")
+
+print("Positive indexing:")
+print("s[0]:", s[0])
+print("s[6]:", s[6])
+
+print("Negative indexing:")
+print("s[-1]:", s[-1])
+print("s[-7]:", s[-7])
+
+
+print("\nAccessing characters by slice operator:")
+print("s[0:6]:", s[0:6])
+print("s[6:]:", s[6:])
+print("s[:6]:", s[:6])
+print("s[-6:]:", s[-6:])
+print("s[::2]:", s[::2])
+print("s[2:11:3]:", s[2:11:3])
