a0330f46889e5ce03f0f3f0455af71e5f1be1040
shamstajbir <25tajbir@gmail.com>
2024-03-16 15:23:51 +0530

    Lab mst Python

---
+++ python6exp.py
@@ -0,0 +1,13 @@
+def get_student_info():
+    name = input("Enter student name: ")
+    age = int(input("Enter student age: "))
+    grade = input("Enter student grade: ")
+
+    return name, age, grade
+
+
+student_name, student_age, student_grade = get_student_info()
+
+print("Student Name:", student_name)
+print("Student Age:", student_age)
+print("Student Grade:", student_grade)
