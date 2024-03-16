46e0b83c0cf63c7a69c890dd4c520356d19890ee
shamstajbir <25tajbir@gmail.com>
2024-03-16 15:42:36 +0530

    second largest in tupple

--- Turtle.py
+++ Turtle.py
@@ -1,21 +1,7 @@
-import turtle
-a = turtle.Turtle()
-a.speed(0)
-a.getscreen().bgcolor("black")
-a.penup()
-a.pendown()
-a.color("orange")
-def star(turtle, size):
-    if size <= 10:
-        return
-    else:
-        turtle.begin_fill()
-        for _ in range(5):
-            turtle.forward(size)
-            star(turtle, size / 3)
-            turtle.left(216)
-        turtle.end_fill()
-a.speed(25)
-star(a, 360)
+# Given tuple
+tupple = (1, 4, 9,9, 8, 7, 6)
 
-turtle.done()
+second_largest = sorted(tupple, reverse=True)[2]
+
+
+print("Second largest element:", second_largest)
