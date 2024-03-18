33953e3eb016d3ad07a270dbc184307374fb61f0
shamstajbir <25tajbir@gmail.com>
2024-03-19 01:16:43 +0530

    exercise tupple

--- AssignmentOperator.py
+++ AssignmentOperator.py
@@ -1,11 +1,21 @@
-num=10
-num+=5
-print("num:",num)
-num-=5
-print("num:",num)
-num*=5
-print("num:",num)
-num/=5
-print("num:",num)
-num%=5
-print("num:",num)
+import turtle
+a = turtle.Turtle()
+a.speed(0)
+a.getscreen().bgcolor("black")
+a.penup()
+a.pendown()
+a.color("orange")
+def star(turtle, size):
+    if size <= 10:
+        return
+    else:
+        turtle.begin_fill()
+        for _ in range(5):
+            turtle.forward(size)
+            star(turtle, size / 3)
+            turtle.left(216)
+        turtle.end_fill()
+a.speed(25)
+star(a, 360)
+
+turtle.done()
