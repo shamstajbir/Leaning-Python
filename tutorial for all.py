23da2b0d003d7a11adf335a506ef86a9125e0dfe
shamstajbir <25tajbir@gmail.com>
2024-04-04 09:07:32 +0530

    Unknown type changing in string type casting

--- tutorial for all.py
+++ tutorial for all.py
@@ -1,4 +1,8 @@
 num_char=len(input("what is your name?"))
 # print("Your name has"+num_char+"characters")
-print(type(num_char))
+#print(type(num_char))
 #যখন জানি না কোন টাইপ এর ইনপুট ,তখন এভাবে করতে হবে
+
+# যখন আগের মত করেই করতে চাইবো তখন একতা কাজ করা লাগবে,সেটা হল string বানাইতে হবে type casting আরকি
+new_num_char=str(num_char)
+print("Your name has"+new_num_char+"characters")
