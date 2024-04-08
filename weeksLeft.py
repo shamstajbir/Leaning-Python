debc326c4510a7f6fbc6cb3d3a9c1d4e8ae2e36c
shamstajbir <25tajbir@gmail.com>
2024-04-08 14:06:07 +0530

    Weeks left of life

---
+++ weeksLeft.py
@@ -0,0 +1,14 @@
+# Create a program using maths and f-Strings that tells us how many weeks we have left, if we live until 90 years old.
+#
+# It will take your current age as the input and output a message with our time left in this format:
+#
+# You have x weeks left.
+# Where x is replaced with the actual calculated number of weeks the input age has left until age 90.
+
+age = input("Write your age:")
+
+all_We_Have=(int(90*52))
+New_age=(int(age))
+New_age*=52
+left_weeks=(int(all_We_Have - New_age))
+print("You have ",left_weeks, "weeks left")
