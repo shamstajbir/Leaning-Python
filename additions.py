5f27f1cac3bd980e79c879ce11b0fe95094494c6
shamstajbir <25tajbir@gmail.com>
2024-04-08 13:28:43 +0530

    int & round making

---
+++ additions.py
@@ -0,0 +1,9 @@
+print(8/3)
+# output=2.6666666666666665
+# normally division
+print(int(8/3))
+#output=2
+# If we want the integer
+print(round(8/3))
+# output=3
+# If we want the round figure আরে ২.৬ মানে তো  ৩ ই
