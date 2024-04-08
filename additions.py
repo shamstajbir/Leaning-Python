b9e58f64513d7965e111cb8072550b273d2371a5
shamstajbir <25tajbir@gmail.com>
2024-04-08 13:31:44 +0530

    without making integer

--- additions.py
+++ additions.py
@@ -1,9 +1,3 @@
-print(8/3)
-# output=2.6666666666666665
-# normally division
-print(int(8/3))
-#output=2
-# If we want the integer
-print(round(8/3))
-# output=3
-# If we want the round figure আরে ২.৬ মানে তো  ৩ ই
+print(8//3)
+# output=2
+# without making the integer
