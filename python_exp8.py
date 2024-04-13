27cbaa745482d7e355f09afacefb02944612067d
shamstajbir <25tajbir@gmail.com>
2024-04-13 23:53:07 +0530

    python_exp8

---
+++ python_exp8.py
@@ -0,0 +1,10 @@
+def get_values():
+    value1 = 10
+    value2 = "Hello"
+    value3 = [1, 2, 3]
+    return value1, value2, value3
+
+result1, result2, result3 = get_values()
+print(result1)
+print(result2)
+print(result3)
