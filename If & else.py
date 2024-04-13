50c291ed1b2e464e05f04ac5e752e2e8c8491d4a
shamstajbir <25tajbir@gmail.com>
2024-04-14 00:39:00 +0530

    Rollercoaster height checker

---
+++ If & else.py
@@ -0,0 +1,6 @@
+print("Welcome to the rollercoaster!")
+height = int(input("What is your height in cm?\n"))
+if height > 120:
+  print("you cann't ride the rollercoaster")
+else:
+  print("Sorry,you have to grow taller before you can ride")
