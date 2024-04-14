16d476c205bb1cd4c682dc0f22097c2362ec67ce
shamstajbir <25tajbir@gmail.com>
2024-04-15 01:03:31 +0530

    roller coaster ticket

--- If & else.py
+++ If & else.py
@@ -1,6 +1,19 @@
-number= int(input("Enter the number:"))
-if number%2==0:
-  print("The  number is even.")
+# number= int(input("Enter the number:"))
+# if number%2==0:
+#   print("The  number is even.")
 
+# else:
+#   print("The number is odd.")
+
+
+print("Welcome to the rollercoaster!")
+height = int(input ("What is your height in cm? "))
+if height >= 120:
+  print("You can ride the rollercoaster!")
+  age = int (input("What is your age? "))
+  if age <= 18:
+    print ("Please pay $7.")
+  else:
+    print ("Please pay $12.")
 else:
-  print("The number is odd.")
+  print("Sorry, you have to grow taller before you can ride.")
