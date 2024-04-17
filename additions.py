314b6c26db1c2a55b5c9f5cb016e07c05bf7c897
shamstajbir <25tajbir@gmail.com>
2024-04-17 11:28:40 +0530

    python exp8

--- additions.py
+++ additions.py
@@ -1,3 +1,10 @@
-print(8//3)
-# output=2
-# without making the integer
+def get_values():
+    value1 = 10
+    value2 = "Hello"
+    value3 = [1, 2, 3]
+    return value1, value2, value3
+
+result1, result2, result3 = get_values()
+print(result1)
+print(result2)
+print(result3)
