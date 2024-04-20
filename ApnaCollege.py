0d8411d03eda8c68db839d8464568e6daf388540
shamstajbir <25tajbir@gmail.com>
2024-04-20 20:45:47 +0530

    tuple

--- ApnaCollege.py
+++ ApnaCollege.py
@@ -1,2 +1,21 @@
-name =input("Write a name:")
-print("press what you want to find :",len(name))
+import turtle
+a = turtle.Turtle()
+a.speed(0)
+a.getscreen().bgcolor("black")
+a.penup()
+a.pendown()
+a.color("orange")
+def star(turtle, size):
+    if size <= 10:
+        return
+    else:
+        turtle.begin_fill()
+        for _ in range(5):
+            turtle.forward(size)
+            star(turtle, size / 3)
+            turtle.left(216)
+        turtle.end_fill()
+a.speed(25)
+star(a, 360)
+
+turtle.done()
