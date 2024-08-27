27fc4df8b50a5eb9d74ecb4f595ab9d1abdbf582
shamstajbir <25tajbir@gmail.com>
2024-08-27 23:54:28 +0530

    jersey2

--- update.py
+++ update.py
@@ -7,7 +7,7 @@ def update_contact(contact_book):
         if (search_key.lower() in contact["name"].lower()):
             print(f"Found index: {idx+1} !!!\n{contact['name']} - {contact['phone']} - {contact['email']}")
 
-    selected_idx = int(input("Enter a contact index to update: "))
+    selected_idx = int(input("Enter a   contact index to update: "))
 
     new_name = contact_book[selected_idx-1]["name"]
     new_phone = contact_book[selected_idx-1]["phone"]
@@ -16,7 +16,7 @@ def update_contact(contact_book):
     up1 = input("Do you want to update the name [y to proceed]: ")
     if (up1 == "y"):
         new_name = input("Enter new name: ")
-       
+
     up2 = input("Do you want to update the phone [y to proceed]: ")
     if (up2 == "y"):
         new_phone = input("Enter new phone: ")
