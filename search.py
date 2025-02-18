a0634f8a1544579c6ff02337cfcbef04bbadddea
shamstajbir <25tajbir@gmail.com>
2025-02-19 02:02:56 +0530

    search_updated 1

---
+++ search.py
@@ -0,0 +1,11 @@
+def search_contact(contact_book):
+    search_key = input("Enter name to search in the model: ")
+
+    flag = False
+    for idx, contact in enumerate(contact_book):
+        if (search_key.lower() in contact["name"].lower()):
+            print(f"index: {idx+1} !!!\n{contact['name']} - {contact['phone']} - {contact['email']}")
+            flag = True
+
+    if (not flag):
+        print("No contact found (-_-)")
