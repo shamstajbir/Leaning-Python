7016500e26f57694b25e3d061f221adecf83ddaf
shamstajbir <25tajbir@gmail.com>
2025-02-19 01:22:17 +0530

    Pizza_Deliveries_updated1

--- pizza Deliveries.py
+++ pizza Deliveries.py
@@ -15,7 +15,7 @@ if height >= 120:
         bill = 12
         print("Adult tickets are $12.")
 
-    wants_photo = input("Do you want a photo taken? Y or N. ")
+    wants_photo = input("Do you want a photo taken??? Y or N. ")
     if wants_photo == "Y":
         bill += 3
 
