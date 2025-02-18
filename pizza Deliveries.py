41996e2b2821ec6e7a7fa0a3dbebfc71b4b156e0
shamstajbir <25tajbir@gmail.com>
2025-02-18 15:08:40 +0530

    Pizza_Deliveries_updated

--- pizza Deliveries.py
+++ pizza Deliveries.py
@@ -1,24 +1,25 @@
-print("Welcome to Python Pizza Deliveries!")
-size = input("What size pizza do you want? S, M or L: ")
-pepperoni = input("Do you want pepperoni on your pizza? Y or N: ")
-extra_cheese = input("Do you want extra cheese? Y or N: ")
+print("Welcome to the rollercoaster!")
+height = int(input("What is your height in cm? "))
+bill = 0
 
-bill=0
-if size =="S":
-    bill+=15
-elif size=="M":
-    bill+=20
-elif size == "L":
-    bill+=25
-else:
-    print("You typed the wrong inputs")
-
-if pepperoni=="Y":
-    if size == "S":
-        bill += 2
+if height >= 120:
+    print("You can ride the rollercoaster!")
+    age = int(input("What is your age? "))
+    if age < 12:
+        bill = 5
+        print("Child tickets are $5.")
+    elif age <= 18:
+        bill = 7
+        print("Youth tickets are $7.")
     else:
-        bill += 3
-if extra_cheese=="Y":
-    bill+=1
-    print(f"Your final bil is :${bill}")
+        bill = 12
+        print("Adult tickets are $12.")
 
+    wants_photo = input("Do you want a photo taken? Y or N. ")
+    if wants_photo == "Y":
+        bill += 3
+
+    print(f"Your final bill is ${bill}")
+
+else:
+    print("Sorry, you have to grow taller before you can ride.")
